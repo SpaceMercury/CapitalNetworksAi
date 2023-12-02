1586aae57ca06847c8f59205e8e7d0962fa13c52
EduardoNeville <eduardoneville82@gmail.com>
2023-12-02 20:10:46 +0100

    fixing utils

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,22 @@
 import yfinance as yf
 import requests
+import json
 
 def ticker_to_isin(ticker):
         return yf.Ticker(ticker).isin
 
 def isin_to_ticker(isin):
-        OpenFIGI_APIKEY="d3251d8b-e60a-4668-b071-a38176ce11d8"
-        url = 'https://api.openfigi.com/v3/mapping'
-        headers = {'Content-Type':'application/json', 'X-OPENFIGI-APIKEY': OpenFIGI_APIKEY}
-        payload = '[{"idType":"ID_ISIN","idValue":"' + isin + '","exchCode":"US"}]'
-
-        return requests.post(url, headers=headers, data=payload)
+    OpenFIGI_APIKEY="d3251d8b-e60a-4668-b071-a38176ce11d8"
+    url = 'https://api.openfigi.com/v3/mapping'
+    headers = {'Content-Type':'application/json', 'X-OPENFIGI-APIKEY': OpenFIGI_APIKEY}
+    payload = '[{"idType":"ID_ISIN","idValue":"'+isin+'","exchCode":"US"}]'
+    response = requests.post(url, headers=headers, data=payload)
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        if data and 'data' in data[0]:
+            return data[0]['data'][0]['ticker']
+    return None
 
 def main():
-    ticker = "AAPL"
+    ticker = "TSLA"
     print(isin_to_ticker(ticker_to_isin(ticker)))
