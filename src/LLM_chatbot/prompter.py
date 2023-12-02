a801f7f9ca4004bea26965f3cc1f08b4bd5d05b7
Pol <polinboston@gmail.com>
2023-12-03 00:06:32 +0100

    prompter advance

--- src/LLM_chatbot/prompter.py
+++ src/LLM_chatbot/prompter.py
@@ -8,18 +8,36 @@ from openai import OpenAI
 
 def main():
 
-    # 1. User asks a question
-    user_input = input("Ask a question: ")
+    #User opens chatbot window
+    window_open = True
+    context_history = None
 
-    # 2. Chatbot processes question with the JSON file of the stock the user is asking about
-    user_question_reccomendation_prompt = make_reccomendation_prompt(user_input);
-    callGPT(user_question_reccomendation_prompt);
+    #infinte loop for user asking questions to bot
+    while(window_open):
+        # 1. User asks a question
+        user_input = input("Ask a question: ")
+
+        # 2. Chatbot processes question with the JSON file of the stock the user is asking about
+        user_reccomendation_prompt = make_reccomendation_prompt(user_input, context_history)
+        chatbot_response = callGPT(user_reccomendation_prompt)
+
+        # 3. Chatbot updates context history
+        context_history = context_history + chatbot_response
+
+        # Display to user
+        print(chatbot_response)
+
+    return 0
 
 
-    return 0;
+def make_reccomendation_prompt(user_input, previous_prompt):
 
+    answer = ""
 
-def make_reccomendation_prompt():
+    #If the user has asked a question, take that into context
+    if(previous_prompt != None):
+        answer = "Here is the previous context you need to know " + previous_prompt
+
 
     ## Get the stock JSON file
     stock_json = open("data/stock_json/AAPL.json", "r")
@@ -27,7 +45,9 @@ def make_reccomendation_prompt():
     ## TODO: Treat JSON data as a string and append it to the prompt
     
 
-    return "The stock we are interested has the following ticker: ‘AAPL’. We also have the stock vector"++"Consider the following user with this characteristic vector:"++". Would you recommend this stock to this specific user, and for what reasons list in order of importance."
+    "The stock we are interested has the following ticker: ‘AAPL’. We also have the stock vector"++"Consider the following user with this characteristic vector:"++". Would you recommend this stock to this specific user, and for what reasons list in order of importance."
+
+    return
 
 def callGPT(prompt):
     ##GPT 4 calling
