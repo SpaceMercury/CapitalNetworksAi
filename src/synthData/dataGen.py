68b7582ee3b4d070e098b9c5108b8d5499ce9bb7
Pol <polinboston@gmail.com>
2023-12-03 03:46:12 +0100

    datagen created and working

--- src/synthData/dataGen.py
+++ src/synthData/dataGen.py
@@ -1,13 +1,27 @@
 from gnews import GNews
+import json
 
 def getStockNews(ticker):
     news = GNews(language='en', max_results=4)
     return news.get_news(ticker)
 
-def main():
-    ticker = "AAPL"
-    news = getStockNews(ticker)
-    print (news)
+def cleanNews(news):
+    for article in news:
+        article.pop('publisher')
+        article.pop('url')      
 
-if __name__ == "__main__":
-    main()
+def add_ticker_news(ticker):
+
+    news = getStockNews(ticker)
+    cleanNews(news)
+
+    # Load the existing data
+    with open(f'data/{ticker}.json', 'r') as f:
+        data = json.load(f)
+
+    # Add the news to the data
+    data['news'] = news
+
+    # Write the data back to the file
+    with open(f'data/{ticker}.json', 'w') as f:
+        json.dump(data, f, indent=4)
