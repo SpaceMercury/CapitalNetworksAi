6a7bea0eccf1e12e9a05a025e49c7bd2efe8b80f
Pol <polinboston@gmail.com>
2023-12-03 01:46:44 +0100

    took out key, working

--- src/LLM_chatbot/prompter.py
+++ src/LLM_chatbot/prompter.py
@@ -3,6 +3,44 @@ import json
 from openai import OpenAI
 
 
+
+
+
+def make_reccomendation_prompt(user_input, previous_prompt):
+
+    answer = str("")
+
+    #If the user has asked a question, take that into context
+    if(previous_prompt == None):
+        answer = previous_prompt
+
+    ## Get the stock JSON file
+    with open ('data/AAPL.json') as f:
+        stock_json = json.load(f)
+
+    ## TODO: Treat JSON data as a string and append it to the prompt
+    answer = f"{answer} {stock_json}"
+
+    answer = answer + "Q: " + user_input
+    return answer
+
+def callGPT(prompt):
+    ##GPT 4 calling
+    client = OpenAI(api_key="")
+    response = client.chat.completions.create(
+        model="gpt-4-1106-preview",
+        messages=[
+            {
+                "role": "user",
+                "content": [{"type": "text", "text": "Given the following context of conversation, of the form Q:{{question}} A:{{answer}} give an answer to the prompt of the user. If there is no context history and just a question, answer it to the best of your abilities. NEVER INCITE A USER TO BUY A STOCK, ONLY RELAY INFORMATION SO THE USER CAN MAKE AN INFORMED DECISION. Remember to be precise in your answers, use detail to help the user to get as much useful information as he can obtain without overwhelming him with info. Here is the context history and the question, the question starts at Q:" + prompt}],
+            }
+        ],
+        max_tokens = 300,
+        )
+    processed_response = response.choices[0].message.content
+    return processed_response
+    
+
 def main():
 
     context_history = None
@@ -24,40 +62,5 @@ def main():
 
     return 0
 
-
-def make_reccomendation_prompt(user_input, previous_prompt):
-
-    answer = ""
-
-    #If the user has asked a question, take that into context
-    if(previous_prompt == None):
-        answer = previous_prompt
-
-    ## Get the stock JSON file
-    stock_json = open("data/stock_json/AAPL.json", "r")
-
-    ## TODO: Treat JSON data as a string and append it to the prompt
-    answer = answer + treat_json_as_string(stock_json)
-
-    answer = answer + "Q: " + user_input
-    return answer
-
-def callGPT(prompt):
-    ##GPT 4 calling
-    client = OpenAI(api_key="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")
-    response = client.chat.completions.create(
-        model="gpt-4-1106-preview",
-        messages=[
-            {
-                "role": "user",
-                "content": [{"type": "text", "text": "Given the following context of conversation, of the form Q:{{question}} A:{{answer}} give an answer to the prompt of the user. If there is no context history and just a question, answer it to the best of your abilities. Remember to be precise in your answers, use detail to help the user to get as much useful information as he can obtain without overwhelming him with info. Here is the context history and the question, the question starts at Q:" + prompt}],
-            }
-        ],
-        max_tokens = 300,
-        )
-    processed_response = response.choices[0].message.content
-    return processed_response
-
-def treat_json_as_string(json_file):
-    json_string = json.dumps(json_file)
-    return json_string
+if __name__ == "__main__":
+    main()
