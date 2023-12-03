517f5ca83c9177dbdc066e25414085c8f1e790ad
Pol <polinboston@gmail.com>
2023-12-03 02:27:16 +0100

    thing

--- src/synthData/dataGen.py
+++ src/synthData/dataGen.py
@@ -1,1 +1,13 @@
+from gnews import GNews
 
+def getStockNews(ticker):
+    news = GNews(language='en', max_results=4)
+    return news.get_news(ticker)
+
+def main():
+    ticker = "AAPL"
+    news = getStockNews(ticker)
+    print (news)
+
+if __name__ == "__main__":
+    main()
